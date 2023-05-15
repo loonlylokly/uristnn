462097793ddd658e912b77cd1ddbad5e3e371fa6
loonlylokly <krivdinm@ya.ru>
2023-05-15 15:48:57 +0300

    feat: create description section for service

--- src/app/(services)/(YuridUslugi)/predstavitelstvo-v-sude/page.tsx
+++ src/app/(services)/(YuridUslugi)/predstavitelstvo-v-sude/page.tsx
@@ -1,10 +1,16 @@
 import HeroService from "@/modules/Service/HeroService/HeroService";
+import Description from "@/modules/Service/Description/Description";
 import { predVSude } from "@/../data/Services/yuriduslugi/pred-v-sude";
 
 export default function PredVSude() {
   return (
     <>
       <HeroService props={predVSude.hero_section} />
+      <Description props={predVSude.description_1} />
+
+      <Description props={predVSude.documents_needed} />
+      <Description props={predVSude.how_work} />
+      <Description props={predVSude.who__need} />
     </>
   )
 }
---
+++ src/modules/Service/Description/Description.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import styles from "./Description.module.css";
+
+type Props = {
+  props: {
+    title?: string;
+    marked?: string;
+    data: (string | Record<string, string[]>)[];
+  }
+};
+
+const Description: FC<Props> = ({props}) => {
+  return (
+    <section className={styles.description}>
+      {props.title && <h2 className={styles.title}>{props.title}</h2>}
+      <span className={styles.text}>
+        {props.marked && <p className={styles.marked}>{props.marked}</p>}
+        {props.data.map((item, index) => {
+          if (typeof(item) !== "string") {
+            if (item.hasOwnProperty('ordered_list')) {
+              return (
+                <ul key={index} className={styles.list}>
+                  {item.ordered_list.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              )
+            }
+            if (item.hasOwnProperty('unordered_list')) {
+              return (
+                <ol key={index} className={styles.list}>
+                  {item.unordered_list.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ol>
+              )
+            }
+          }
+          return (<p key={item.toString()}>{item.toString()}</p>);
+        })}
+      </span>
+    </section>
+  )
+}
+
+export default Description;
