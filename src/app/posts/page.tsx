2ea210aad500020a7affe59496949f7f45cee536
loonlylokly <krivdinm@ya.ru>
2023-09-18 16:19:36 +0300

    fix: metaTags and FAQ marker

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,15 @@
-import Link from 'next/link';
+import { Metadata } from 'next';
 import List from '@/components/Lists/List/List';
 import PostCard from '@/components/Cards/PostCard/PostCard';
-import styles from './page.module.css';
-import { PostMetadata } from '@/../types/PostMetadata';
 import Breadcrumbs from '@/modules/Breadcrumbs/Breadcrumbs';
+import { PostMetadata } from '@/../types/PostMetadata';
 import { getPostMetadata } from '@/lib/GetMetadata/getPostMetadata';
+import styles from './page.module.css';
+
+export const metadata: Metadata = {
+  title: 'Статьи «Статский Советник» Юридическое агенство',
+  description: 'Юридическая помощь в Нижнем Новгороде быстро и профессионально. Юристы с опытом от 10 лет. Срочная помощь. Контактная информация, адрес, электронная почта. Звоните нам по номеру ☎ +7 (831) 4…',
+}
 
 export default function Posts() {
   const postsMetadata = getPostMetadata('./posts');
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from 'next';
 import Breadcrumbs from '@/modules/Breadcrumbs/Breadcrumbs';
 import ListServices from '@/modules/ListServices/ListServices';
-import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Услуги «Статский Советник» Юридическое агенство',
+  description: 'Юридическая помощь в Нижнем Новгороде быстро и профессионально. Юристы с опытом от 10 лет. Срочная помощь. Контактная информация, адрес, электронная почта. Звоните нам по номеру ☎ +7 (831) 4…',
+}
 
 export default function Service() {
   return (
